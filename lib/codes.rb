39484111694069396120360119adbec0452ddd93
slimecog <wlcjohnson@gmail.com>
2017-12-03 16:18:12 -0700

    idiomatic variable update

--- lib/codes.rb
+++ lib/codes.rb
@@ -1,22 +1,22 @@
 module Codes
 
   def easy_code
-    x = []
-    4.times { x << rand(1..4) }
-    x = x.join.gsub("1", "r").gsub("2", "g").gsub("3", "b").gsub("4", "y")
+    code_array = []
+    4.times { code_array << rand(1..4) }
+    code_array = code_array.join.gsub("1", "r").gsub("2", "g").gsub("3", "b").gsub("4", "y")
   end
 
   def medium_code
-    x = []
-    6.times { x << rand(1..5) }
-    x = x.join.gsub("1", "r").gsub("2", "g").gsub("3", "b").gsub("4", "y")
-    x.gsub("5", "o")
+    code_array = []
+    6.times { code_array << rand(1..5) }
+    code_array = code_array.join.gsub("1", "r").gsub("2", "g").gsub("3", "b").gsub("4", "y")
+    code_array.gsub("5", "o")
   end
 
   def hard_code
-    x = []
-    8.times { x << rand(1..6) }
-    x = x.join.gsub("1", "r").gsub("2", "g").gsub("3", "b").gsub("4", "y")
-    x.gsub("5", "o").gsub("6", "p")
+    code_array = []
+    8.times { code_array << rand(1..6) }
+    code_array = code_array.join.gsub("1", "r").gsub("2", "g").gsub("3", "b").gsub("4", "y")
+    code_array.gsub("5", "o").gsub("6", "p")
   end
 end
